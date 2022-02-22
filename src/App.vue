7ef7431805114ed3913e337c29a675166ff1d506
Peter-Bence Kompasz <kompaszbence@gmail.com>
2022-02-22 10:10:18 +0200

    cleanup App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,66 @@
 	<div>
 		<h4>Unit Select</h4>
 		<div>
-			<input type="radio" id="one" value="Dings" v-model="picked"/>
-			<label for="one">Dings</label>
+			<input 
+                type="radio"
+                id="one" 
+                value="Dings" 
+                v-model="picked"
+            />
+			<label 
+                for="one"
+            >Dings</label>
 			<br/>
-			<input type="radio" id="two" value="Da" v-model="picked"/>
-			<label for="two">Da</label>
-			<br/>
-			<span>Current Unit: {{ picked }}</span><br/>
+			<input 
+                type="radio" 
+                id="two" 
+                value="Da" 
+                v-model="picked"
+            />
+			<label 
+                for="two"
+            >Da</label>
+            <br/>
+            <span>
+                Current Unit: {{ picked }}
+            </span>
+            <br/>
 			<br/>
 
-			<input type="radio" id="horizontal" value="horizontal" v-model="orientation"/>
-			<label for="one">Horizontal</label>
+			<input 
+                type="radio" 
+                id="horizontal" 
+                value="horizontal"
+                v-model="orientation"
+            />
+			<label 
+                for="one"
+            >Horizontal</label>
 			<br/>
-			<input type="radio" id="vertical" value="vertical" v-model="orientation"/>
-			<label for="two">Vertical</label>
+			<input 
+                type="radio" 
+                id="vertical" 
+                value="vertical" 
+                v-model="orientation"
+            />
+			<label 
+                for="two"
+            >Vertical</label>
 		</div>
 		<br>
-		<h4>None</h4>
-		<calendar-heatmap :values="[]" :end-date="endDate" :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}"
-						  :vertical="orientation === 'vertical'" :no-data-text="false"/>
+		<h4>
+            None
+        </h4>
+		<calendar-heatmap 
+            :values="[]" 
+            :end-date="endDate" 
+            :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}"
+            :vertical="orientation === 'vertical'" :no-data-text="false"
+        />
 		<br>
-		<h4>Some</h4>
+		<h4>
+            Some
+        </h4>
 		<calendar-heatmap
 			:values="values"
 			:end-date="endDate"
@@ -31,29 +70,57 @@
 			no-data-text="NOTHING"
 		/>
 		<br>
-		<h4>Some (rounded corners)</h4>
-		<calendar-heatmap :values="values" :end-date="endDate" :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" :round="2"
-						  :vertical="orientation === 'vertical'"/>
+		<h4>
+            Some (rounded corners)
+        </h4>
+		<calendar-heatmap 
+            :values="values" 
+            :end-date="endDate" 
+            :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" 
+            :round="2"
+            :vertical="orientation === 'vertical'"
+        />
 		<br>
-		<h4>Some (circles)</h4>
-		<calendar-heatmap :values="values" :end-date="endDate" :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" :round="5"
-						  :vertical="orientation === 'vertical'"/>
+		<h4>
+            Some (circles)
+        </h4>
+		<calendar-heatmap 
+            :values="values" 
+            :end-date="endDate" 
+            :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" 
+            :round="5"
+            :vertical="orientation === 'vertical'"
+        />
 		<br>
-		<h4>Locale</h4>
-		<calendar-heatmap :values="values" :end-date="endDate" :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" :locale="{
-			months: [ 'Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez' ],
-			days  : [ 'So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa' ],
-			on    : 'am',
-			less  : 'Weniger',
-			more  : 'Mehr'
-
-		}" :vertical="orientation === 'vertical'"/>
+		<h4>
+            Locale
+        </h4>
+		<calendar-heatmap 
+            :values="values" 
+            :end-date="endDate" 
+            :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" 
+            :locale="{
+                months: [ 'Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez' ],
+                days  : [ 'So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa' ],
+                on    : 'am',
+                less  : 'Weniger',
+                more  : 'Mehr'
+            }" :vertical="orientation === 'vertical'"/>
 		<br>
-		<h4>Tooltip Unit</h4>
-		<calendar-heatmap :values="values" :end-date="endDate" :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" :tooltip-unit="picked"
-						  :vertical="orientation === 'vertical'"/>
+		<h4>
+            Tooltip Unit
+        </h4>
+		<calendar-heatmap 
+            :values="values" 
+            :end-date="endDate" 
+            :style="{'max-width': orientation === 'vertical' ? '145px' :  '675px'}" 
+            :tooltip-unit="picked"
+            :vertical="orientation === 'vertical'"
+        />
 		<br>
-		<h4>TooltipFormatter</h4>
+		<h4>
+            TooltipFormatter
+        </h4>
 		<calendar-heatmap
 			:values="values"
 			:end-date="endDate"
