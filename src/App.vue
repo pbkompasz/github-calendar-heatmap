0b7252d424527faac56101b5c7d8eb53918c7bad
Peter Kompasz <kompaszbence@gmail.com>
2022-12-09 21:31:13 +0200

    New options\

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,75 @@
 <template>
   <div>
-    <h4>Unit Select</h4>
     <div>
-      <input type="radio" id="one" value="Dings" v-model="picked" />
-      <label for="one">Dings</label>
+
+      <h4>Unit Select</h4>
+      <input type="radio" id="two" value="contributions" v-model="picked" />
+      <label for="one">Default(contributions)</label>
       <br />
       <input type="radio" id="two" value="Da" v-model="picked" />
       <label for="two">Da</label>
       <br />
-      <span>
-        Current Unit: {{ picked }}
-      </span>
-      <br />
+      <input type="radio" id="one" value="Dings" v-model="picked" />
+      <label for="two">Dings</label>
       <br />
 
+      <h4>Orientation</h4>
       <input type="radio" id="horizontal" value="horizontal" v-model="orientation" />
       <label for="one">Horizontal</label>
       <br />
       <input type="radio" id="vertical" value="vertical" v-model="orientation" />
       <label for="two">Vertical</label>
+      <br />
+
+      <h4>Darkmode</h4>
+      <input type="checkbox" id="horizontal" value="horizontal" v-model="darkmode" />
+      <label for="one">Darkmode</label>
+      <br />
+
+      <h4>Colors</h4>
+      <span>Light mode: </span>
+      <input type="text" v-model="lightColor">
+      <br />
+      <span>Dark mode: </span>
+      <input type="text" v-model="darkColor">
+
     </div>
     <br>
     <h4>None</h4>
-    <calendar-heatmap :values="[]" :end-date="endDate"
-      :vertical="orientation === 'vertical'"
-      :no-data-text="false" />
+    <calendar-heatmap :values="[]" :end-date="endDate" :vertical="orientation === 'vertical'" :no-data-text="false" />
     <br>
     <h4>Some</h4>
-    <calendar-heatmap :values="values" :end-date="endDate"
-      :vertical="orientation === 'vertical'"
+    <calendar-heatmap :values="values" :end-date="endDate" :vertical="orientation === 'vertical'"
       no-data-text="NOTHING" />
     <br>
     <h4>Some (rounded corners)</h4>
-    <calendar-heatmap :values="values" :end-date="endDate"
-      :round="2"
-      :vertical="orientation === 'vertical'" />
+    <calendar-heatmap :values="values" :end-date="endDate" :round="2" :vertical="orientation === 'vertical'" />
     <br>
     <h4>Some (circles)</h4>
-    <calendar-heatmap :values="values" :end-date="endDate"
-      :round="5"
-      :vertical="orientation === 'vertical'" />
+    <calendar-heatmap :values="values" :end-date="endDate" :round="5" :vertical="orientation === 'vertical'" />
     <br>
     <h4>Locale</h4>
-    <calendar-heatmap :values="values" :end-date="endDate"
-      :locale="{
-        months: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
-        days: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-        on: 'am',
-        less: 'Weniger',
-        more: 'Mehr'
-      }" :vertical="orientation === 'vertical'" />
+    <calendar-heatmap :values="values" :end-date="endDate" :locale="{
+      months: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+      days: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+      on: 'am',
+      less: 'Weniger',
+      more: 'Mehr'
+    }" :vertical="orientation === 'vertical'" />
     <br>
     <h4>Tooltip Unit</h4>
-    <calendar-heatmap :values="values" :end-date="endDate"
-      :tooltip-unit="picked"
+    <calendar-heatmap :values="values" :end-date="endDate" :tooltip-unit="picked"
       :vertical="orientation === 'vertical'" />
     <br>
     <h4>TooltipFormatter</h4>
     <calendar-heatmap :values="values" :end-date="endDate"
-            :tooltip-formatter="(c, u) => c.count ? (c.count / 3600 / 1000) + ' ' + u : 'NÖX'" no-data-text="NIX"
+      :tooltip-formatter="(c, u) => c.count ? (c.count / 3600 / 1000) + ' ' + u : 'NÖX'" no-data-text="NIX"
       :tooltip-unit="picked" :vertical="orientation === 'vertical'" />
     <br>
     <h4>
       Slots
     </h4>
-    <calendar-heatmap :values="[{ date: '2022-1-22', count: 6 },]"
-      :vertical="orientation === 'vertical'"
+    <calendar-heatmap :values="[{ date: '2022-1-22', count: 6 },]" :vertical="orientation === 'vertical'"
       :darkMode="false">
       <template #tooltip-active>
         <div>Active message</div>
@@ -94,15 +98,13 @@
     <h4>
       Disable interaction
     </h4>
-    <calendar-heatmap :values="[{ date: '2022-1-22', count: 6 },]"
-      :vertical="orientation === 'vertical'"
+    <calendar-heatmap :values="[{ date: '2022-1-22', count: 6 },]" :vertical="orientation === 'vertical'"
       :darkMode="false" :noInteract="true">
     </calendar-heatmap>
     <h4>
       Legend orientation
     </h4>
-    <calendar-heatmap :values="[{ date: '2022-1-22', count: 6 },]"
-      :vertical="orientation === 'vertical'"
+    <calendar-heatmap :values="[{ date: '2022-1-22', count: 6 },]" :vertical="orientation === 'vertical'"
       :darkMode="false" :legendDirectionReverse="true">
     </calendar-heatmap>
   </div>
@@ -113,12 +115,16 @@ import { ref } from 'vue';
 import GithubCalendarHeatmap from '@/components/CalendarHeatmap.vue';
 import CalendarHeatmap from '@/components/CalendarHeatmap.vue';
 import { data } from './data';
+import { Heatmap } from './components/Heatmap';
 
 const values = ref(data);
-const endDate =  ref(new Date());
-const picked = ref('Dings');
+const endDate = ref(new Date());
+const picked = ref('contributions');
 const orientation = ref('horizontal');
 const hex = ref();
+const darkmode = ref(false)
+const lightColor = ref(Heatmap.DEFAULT_RANGE_COLOR_LIGHT[4])
+const darkColor = ref(Heatmap.DEFAULT_RANGE_COLOR_DARK[4])
 </script>
 
 <style>
