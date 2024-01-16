98e6ea5cf03c2ac21776e5c7534555fc7c7900fe
Peter Kompasz <kompaszbence@gmail.com>
2024-01-16 17:48:36 +0200

    heatmap: Create Vue component

--- src/heatmap/heatmap.ts
+++ src/heatmap/heatmap.ts
@@ -1,13 +1,23 @@
-import { getLocale } from "../util/util";
-import { DAYS_IN_ONE_YEAR, DEFAULT_RANGE_COLOR_LIGHT, SQUARE_SIZE } from "./defaults";
+import { h } from "vue";
+import {
+  RANGE_COLOR_LIGHT,
+  LOCALE,
+  MAX,
+  RANGE_COLOR_DARK,
+  SQUARE_SIZE,
+  WEEK_FIRST_DAY,
+  WEEKS_GAP,
+  MONTHS_GAP,
+} from "./defaults";
 import type {
   Activities,
   Calendar,
   Color,
   Contribution,
+  DayOfWeek,
   Locale,
   Month,
-  Value,
+  Style,
 } from "./types";
 
 export class Heatmap {
@@ -18,6 +28,8 @@ export class Heatmap {
   months: number;
   locale: Locale;
   colorPalette: Color[];
+  weekStartDay: DayOfWeek;
+  darkmode: boolean;
 
   private _contributions: Contribution[];
   private _firstFullWeekOfMonths?: Month[];
@@ -28,9 +40,11 @@ export class Heatmap {
     contributions: Contribution[],
     endDate: Date,
     startDate: Date,
-    max: number,
-    locale: Locale,
-    colorPalette: Color[]
+    max: number = MAX,
+    locale: Locale = LOCALE,
+    darkmode: boolean,
+    colorPalette: Color[],
+    weekStartDay: DayOfWeek = WEEK_FIRST_DAY
   ) {
     // contributions.sort((c1: Contribution, c2: Contribution) => {
     //   return +new Date(c1.date) - +new Date(c2.date);
@@ -42,7 +56,10 @@ export class Heatmap {
     this.max = max;
     this.months = 12;
     this.locale = locale;
-    this.colorPalette = colorPalette;
+    this.colorPalette =
+      colorPalette ?? darkmode ? RANGE_COLOR_LIGHT : RANGE_COLOR_DARK;
+    this.weekStartDay = weekStartDay;
+    this.darkmode = darkmode;
   }
 
   set contributions(c: Contribution) {
@@ -61,17 +78,17 @@ export class Heatmap {
     return [];
   }
 
-  public static createCalendarObject(heatmap: Heatmap) {
-    const { startDate, endDate } = heatmap;
+  public static createCalendarDescriptor(heatmap: Heatmap) {
+    const { startDate, endDate, weekStartDay } = heatmap;
     const calendar = [];
 
     // First "maybe" partial week
-    let numberInWeek = new Date(startDate).getDay();
-    console.log(new Date(startDate))
-    console.log(new Date(endDate))
+    let numberInWeek = new Date(startDate).getDay() - 1;
+    console.log(new Date(startDate));
+    console.log(new Date(endDate));
     for (let i = numberInWeek; i <= 6; i++) {
       // TODO 1 is margin
-      calendar.push([0, i * (SQUARE_SIZE + 1)]);
+      calendar.push([WEEKS_GAP, MONTHS_GAP + i * (SQUARE_SIZE + 1)]);
     }
 
     // Full weeks between
@@ -82,44 +99,101 @@ export class Heatmap {
     const weeksInBetween = Math.round(days / 7);
     for (let i = 1; i <= weeksInBetween; i++) {
       for (let j = 0; j <= 6; j++) {
-        calendar.push([i * (SQUARE_SIZE + 1), j * (SQUARE_SIZE + 1)]);
+        calendar.push([
+          WEEKS_GAP + i * (SQUARE_SIZE + 1),
+          MONTHS_GAP + j * (SQUARE_SIZE + 1),
+        ]);
       }
     }
 
     // Last "maybe" partial week
-    numberInWeek = endDate.getDay();
+    // TODO Add startOfTheWeek
+    numberInWeek = endDate.getDay() + 1;
     console.log(numberInWeek);
     for (let i = 0; i <= numberInWeek; i++) {
       // TODO 1 is margin
-      calendar.push([(weeksInBetween + 1) * (SQUARE_SIZE + 1), i * (SQUARE_SIZE + 1)]);
+      calendar.push([
+        WEEKS_GAP + (weeksInBetween + 1) * (SQUARE_SIZE + 1),
+        MONTHS_GAP + i * (SQUARE_SIZE + 1),
+      ]);
     }
 
     return calendar;
   }
 
-  public static drawCalendar(
+  // NOTE svgContgainer
+  public static createCalendarHTML(
     calendar: number[][],
-    width: number,
-    height: number
+    style: Style,
+    containerWidth: number | string = "100%",
+    containerHeight: number | string = "100%",
+    heatmapWidth: number | string = "700px",
+    heatmapHeight: number | string = "150px"
   ) {
-    const ns = 'http://www.w3.org/2000/svg'
-    const svg = document.createElementNS(ns, "svg");
-    svg.setAttributeNS(null, "width", String(width));
-    svg.setAttributeNS(null, "height", String(height));
+    const rectangles = [];
+
     for (let r of calendar) {
-      const rect = document.createElementNS(ns, "rect");
-      rect.setAttributeNS(null, "height", String(SQUARE_SIZE));
-      rect.setAttributeNS(null, "width", String(SQUARE_SIZE));
-      rect.setAttributeNS(null, "x", String(r[0]));
-      rect.setAttributeNS(null, "y", String(r[1]));
-      rect.setAttributeNS(null, "fill", DEFAULT_RANGE_COLOR_LIGHT[0]);
-      rect.style.margin = '1px';
-      rect.addEventListener('mouseover', () => {
-        console.log(`[${r[0]}] [${r[1]}]`)
-      })
-      svg.appendChild(rect);
+      const rect = h("rect", {
+        height: String(SQUARE_SIZE),
+        width: String(SQUARE_SIZE),
+        x: String(r[0]),
+        y: String(r[1]),
+        fill: style.colorPalette[0],
+        style: {
+          margin: "1px",
+        },
+        onMouseover() {
+          console.log(`[${r[0]}] [${r[1]}]`)
+        }
+      });
+      rectangles.push(rect);
     }
 
-    return svg;
+    const svg = h(
+      "svg",
+      {
+        style: {
+          width: heatmapWidth,
+          height: heatmapHeight,
+        },
+      },
+      rectangles
+    );
+
+    const svgContainer = h(
+      "div",
+      {
+        style: {
+          overflowX: "scroll",
+          width: '100%',
+        },
+      },
+      [svg]
+    );
+
+    const divBottom = h(
+      "div",
+      {
+        style: {
+          width: "100%",
+          height: "50px",
+          border: '1px solid'
+        },
+      },
+      "Bottom shit"
+    );
+
+    const container = h(
+      "div",
+      {
+        style: {
+          width: containerWidth,
+          height: containerHeight,
+        },
+      },
+      [svgContainer, divBottom]
+    );
+
+    return container;
   }
 }
